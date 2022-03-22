dcc5a41607ebffd2ae6f73b505717fc47f900367
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-22 16:24:10 +0430

    refactor layout routing module

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
-import { MaterialModule } from '../shared/modules/material.module';
-import { PrimeNgModule } from '../shared/modules/prime-ng.module';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { MenuComponent } from './components/menu/menu.component';
 
