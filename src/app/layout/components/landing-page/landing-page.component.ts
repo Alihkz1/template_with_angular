9523c0361d59e16b01113280504a21bf70c37f43
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-22 15:28:29 +0430

    refactor landing-page

--- src/app/layout/components/landing-page/landing-page.component.ts
+++ src/app/layout/components/landing-page/landing-page.component.ts
@@ -7,11 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  public value: number = 0;
+  public started: boolean = false;
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   public onStart() {
-    this.router.navigate(['/menu']);
+    this.started = true;
+    setTimeout(() => {
+      this.value = 20;
+    }, 200);
+    setTimeout(() => {
+      this.value = 40;
+    }, 400);
+    setTimeout(() => {
+      this.value = 60;
+    }, 600);
+    setTimeout(() => {
+      this.value = 80;
+    }, 800);
+    setTimeout(() => {
+      this.router.navigate(['/menu']);
+    }, 1000);
   }
 }
