0844a3273c6988f1273995b6eace410ffd8ab92f
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-22 20:32:59 +0430

    refactor

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './components/container/container.component';
-import { MaterialModule } from '../shared/modules/material.module';
-import { PrimeNgModule } from '../shared/modules/prime-ng.module';
+import { MaterialModule } from '../../shared/modules/material.module';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 
 @NgModule({
   declarations: [ContainerComponent, LandingPageComponent, MenuComponent],
-  imports: [CommonModule, MaterialModule, PrimeNgModule, LayoutRoutingModule],
+  imports: [CommonModule, MaterialModule, LayoutRoutingModule],
   exports: [LayoutRoutingModule],
 })
 export class LayoutModule {}
