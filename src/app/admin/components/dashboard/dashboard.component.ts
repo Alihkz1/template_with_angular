4fc7896aa3645fb8915bfd2ecc5bc1445317fbb1
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-24 01:13:57 +0430

    refactor dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -8,6 +8,9 @@ import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  color: ThemePalette = 'accent';
+  mode: ProgressSpinnerMode = 'determinate';
+
   public row1_cards: any[] = [
     {
       title: 'Active users',
@@ -29,22 +32,19 @@ export class DashboardComponent implements OnInit {
 
   public row2_cards: any[] = [
     {
-      title: 'Shiba Inu',
+      title: 'Sold Product',
       subTitle: 'Dog Breed',
       p: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting',
       value: 50,
     },
     {
-      title: 'Shiba Inu',
+      title: 'Customer Rate',
       subTitle: 'Dog Breed',
       p: 'The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting',
       value: 80,
     },
   ];
   constructor() {}
-
-  color: ThemePalette = 'accent';
-  mode: ProgressSpinnerMode = 'determinate';
 
   ngOnInit(): void {}
 }
