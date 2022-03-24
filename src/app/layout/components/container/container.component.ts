fff23511e18a962de777088be78ab87e86234383
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-25 02:32:56 +0430

    refactor poject

--- src/app/layout/components/container/container.component.ts
+++ src/app/layout/components/container/container.component.ts
@@ -8,14 +8,28 @@ import { ROUTING } from 'src/shared/enums/routing.enum';
   styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent implements OnInit {
+  public value: number = 0;
   public darkMode: boolean = false;
+  public sideBarBtnsClicked: boolean = false;
 
   constructor(private router: Router) {}
 
   @HostBinding('class') get themeMode() {
     return this.darkMode ? 'dark-theme' : 'light-theme';
   }
+
   ngOnInit(): void {
     this.router.navigate([ROUTING.MENU]);
+  }
+
+  public showProgressBar(): void {
+    this.sideBarBtnsClicked = true;
+    let interval = setInterval(() => {
+      this.value += 10;
+      if (this.value === 100) {
+        clearInterval(interval);
+      }
+    }, 50);
+    this.value = 0;
   }
 }
