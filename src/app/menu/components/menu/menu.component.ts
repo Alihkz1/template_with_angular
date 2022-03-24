7513fbbf5cb7a865a29a4c0ef3aa05b93fa3addb
Ali Hkz <aliho3einkhani1@gmail.com>
2022-03-24 20:04:40 +0430

    feat : progress bar for menu

--- src/app/menu/components/menu/menu.component.ts
+++ src/app/menu/components/menu/menu.component.ts
@@ -8,6 +8,9 @@ import { ROUTING } from 'src/shared/enums/routing.enum';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  public value: number = 0;
+  public started: boolean = false;
+
   public cards: any[] = [
     {
       title: 'Weather Check',
@@ -36,7 +39,14 @@ export class MenuComponent implements OnInit {
   constructor(private router: Router) {}
 
   public onRouteTo(route: any) {
-    this.router.navigate([route]);
+    this.started = true;
+    let interval = setInterval(() => {
+      this.value += 5;
+      if (this.value === 100) {
+        clearInterval(interval);
+        this.router.navigate([route]);
+      }
+    }, 50);
   }
 
   ngOnInit(): void {}
