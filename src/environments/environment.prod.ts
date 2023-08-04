f88457215a6a8dea5415b6e9258bc8339a3cff2a
Ali Hosseinkhani <81155890+Alihkz1@users.noreply.github.com>
2023-08-04 15:59:33 -0700

    fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  weather_base: 'https://api.openweathermap.org/data/2.5/weather?q=',
+  weather_apiKey: '&limit=5&appid=' + 'cb2201a199894daaf5cd664edb1f49ae',
 };
